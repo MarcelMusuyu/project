0d8a9b5ed13a90c779d3d5b1ea0be180ff8784d4
marcel Musuyu <marcelmusuyu9@gmail.com>
2024-10-24 16:10:37 +0200

    Updating javascript functions

--- scripts/project-details.js
+++ scripts/project-details.js
@@ -1,3 +1,27 @@
+const lastModified = document.lastModified;
+const formattedDateTime = new Date(lastModified).toLocaleString();
+
+last.innerHTML = `Last modification ${formattedDateTime}`;
+
+const year = document.querySelector("#currentyear");
+year.innerHTML = `${today.getFullYear()}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
+
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+    document
+      .querySelectorAll("a")
+      .forEach((link) => link.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
 const projectID = window.localStorage.getItem("projectID") || "";
 let projectObject = null;
 // 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
